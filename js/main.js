42ca3d844b17b77f3575b76f5662eb13f2d78519
C-A de Salaberry <cadesalaberry@yahoo.com>
2017-07-29 21:05:50 +0200

    Upd: Display one badge per value returned.

--- js/main.js
+++ js/main.js
@@ -10,41 +10,50 @@ function card_badges(t, options) {
     .then(function extract(cardName){
       console.log('We just loaded the card name for fun: ' + cardName);
       var badges = [];
+      // cardName = 'A test card [3] [33] (44) (4)';
 
-      var parenthesis = new RegExp(/.*\((\d*)\).*/);
-      var brackets = new RegExp(/.*\[(\d*)\].*/);
+      var parenthesis = new RegExp(/.*\((\d+)\).*/);
+      var brackets = new RegExp(/.*\[(\d+)\].*/);
 
-      var consumed = parenthesis.exec(name);
-      var total = brackets.exec(name);
+      var consumed = brackets.exec(cardName)[1];
+      var total = parenthesis.exec(cardName)[1];
 
       console.log('[time-tracker]', consumed, '/', total);
 
-      if (consumed) {
+      for (var i = consumed.length - 1; i >= 1; i--) {
+        var consumedTime = consumed[i];
+
         badges.push({
-          dynamic: function getConsumedTime(){
-            return {
-              text: consumed,
-              // icon: './images/icon.svg',
-              color: 'green', // Valid values: 'green', 'yellow', 'red', 'none'
-              refresh: 1 // in seconds
-            };
-          },
+          dynamic: getConsumedTimeBadge.bind(null, consumedTime),
         });
       }
 
-      if (total) {
+      for (var j = consumed.length - 1; j >= 1; j--) {
+        var totalTime = consumed[j];
+
         badges.push({
-          dynamic: function getConsumedTime(){
-            return {
-              text: total,
-              // icon: './images/icon.svg',
-              color: 'red', // Valid values: 'green', 'yellow', 'red', 'none'
-              refresh: 1 // in seconds
-            };
-          },
+          dynamic: getTotalTimeBadge.bind(null, totalTime),
         });
       }
 
       return badges;
+
+      function getConsumedTimeBadge(time){
+        return {
+          text: time,
+          // icon: './images/icon.svg',
+          color: 'green', // Valid values: 'green', 'yellow', 'red', 'none'
+          refresh: 1 // in seconds
+        };
+      }
+
+      function getTotalTimeBadge(time){
+        return {
+          text: time,
+          // icon: './images/icon.svg',
+          color: 'red', // Valid values: 'green', 'yellow', 'red', 'none'
+          refresh: 1 // in seconds
+        };
+      }
     });
 }
