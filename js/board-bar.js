d43f95911fa6089130a4fc9c80b548aa03c0d7ce
C-A de Salaberry <cadesalaberry@yahoo.com>
2017-07-30 15:45:45 +0200

    Add: Get all cards.

--- js/board-bar.js
+++ js/board-bar.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('[time-tracker]', 'board-bar loaded.');
+
+/* global TrelloPowerUp */
+
+var t = TrelloPowerUp.iframe();
+
+// this function we be called once on initial load
+// and then called each time something changes that
+// you might want to react to, such as new data being
+// stored with t.set()
+t.render(function(){
+  return t.cards('all')
+    .then(function (cards) {
+      console.log(JSON.stringify(cards, null, 2));
+    });
+});
