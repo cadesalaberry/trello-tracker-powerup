c6b26c42b887d75a2eef41072b03dc758539452f
C-A de Salaberry <cadesalaberry@yahoo.com>
2017-07-30 16:01:28 +0200

    Add: Log my cards.

--- js/board-bar.js
+++ js/board-bar.js
@@ -13,6 +13,29 @@ var t = TrelloPowerUp.iframe();
 t.render(function(){
   return t.cards('all')
     .then(function (cards) {
+      var context = t.getContext();
+      var memberId = context.member;
+
+      var myCards = cards.filter(getMyCards);
+
+      var cardNames = myCards.map(getName);
+
+      var div = document.getElementById('my-cards');
+
+      div.textContent = cardNames.join('\n');
+
       console.log(JSON.stringify(cards, null, 2));
+
+      function getName(c) {
+        return c.name;
+      }
+
+      function getMyCards(card) {
+        return card.members.filter(getMe);
+
+        function getMe(member) {
+          return member.id === memberId;
+        }
+      }
     });
 });
